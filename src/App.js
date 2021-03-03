2b3377c1ae11e8512f42e9fa1e80fbfbe5cf8f38
Rahull <rahull_@Ajencys-MacBook-Air.local>
2021-03-03 10:04:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Home from './screens/home/home';
+import Men from './screens/men/men';
+import Women from './screens/women/women';
+import Cart from './screens/cart/cart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+  Link
+} from "react-router-dom";
+
+
+class App extends React.Component {
+  
+  render(){
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+                <Route exact path="/catalog/men">
+                  <Men name='Men' />
+                </Route>
+                <Route exact path="/catalog/women">
+                  <Women name='Women' />
+                </Route>
+                <Route exact path='/cart' >
+                  <Cart />
+                </Route>
+                <Route exact path='/' >
+                  <Home />
+                </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
